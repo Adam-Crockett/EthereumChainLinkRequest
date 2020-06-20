37686dec1268fac80eab29ef39f8d63af1afd144
Adam Crockett <arc1224@gmail.com>
2020-06-19 19:51:22 -0700

    readme update

--- GasPriceContract.sol
+++ GasPriceContract.sol
@@ -1,18 +1,21 @@
 pragma solidity ^0.6.0;
 
+// Used this import statement while working within Remix
 import "https://github.com/smartcontractkit/chainlink/evm-contracts/src/v0.6/ChainlinkClient.sol";
 
-// Contract to request the current gas price using the Google BigQuery API
+// Contract to request the current gas price using the Google BigQuery API.
 contract GasPriceContract is ChainlinkClient {
     uint256 oraclePayment;
     address public owner;
     int256 public gasPrice;
 
+    // Create the public LINK token for the network
     constructor(uint256 _oraclePayment) public {
         setPublicChainlinkToken();
         oraclePayment = _oraclePayment;
     }
 
+    // Build and send Chainlink Request
     function requestGasPriceByBlock(
         address _oracle,
         bytes32 _jobId,
@@ -23,12 +26,15 @@ contract GasPriceContract is ChainlinkClient {
             address(this),
             this.fulfillGasPrice.selector
         );
+        // Parameters for request
         req.add("action", "block");
         req.addUint("block", _block);
         req.add("copyPath", "gasPrice");
+
         sendChainlinkRequestTo(_oracle, req, oraclePayment);
     }
 
+    // Sets the gasPrice from the response fulfillment
     function fulfillGasPrice(bytes32 _requestId, int256 _gasPrice)
         public
         recordChainlinkFulfillment(_requestId)
@@ -36,6 +42,7 @@ contract GasPriceContract is ChainlinkClient {
         gasPrice = _gasPrice;
     }
 
+    // Cancel Contract if it is not fullfilled
     function cancelRequest(
         bytes32 _requestId,
         uint256 _payment,
@@ -50,6 +57,7 @@ contract GasPriceContract is ChainlinkClient {
         );
     }
 
+    // Withdaw the contract if there are extra LINKS on contract
     function withdrawLink() public onlyOwner {
         LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
         require(
@@ -58,6 +66,7 @@ contract GasPriceContract is ChainlinkClient {
         );
     }
 
+    // Ensure that the sender of the contract is the owner.
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# EtheriumChainLinkRequest
+# EthereumChainLinkRequest
+
+Created two contracts on the Ethereum.
+The first was a basic run through to request the Ethereum Price in USD.
+
+The second contract was to use the BigQuery API from Google to request the current Gas Price.
+
+Learning points:
+
+- How to implement a basic contract on the Ethereum Network
+- What are the basic parts of a contract and the requirements to maintain security
+- Using Solidity
